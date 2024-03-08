034a18d927a9177887de48896c512701e179032a
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-03-09 02:51:36 +0600

    fixed: redirect auth

--- app/providers/auth.provider.tsx
+++ app/providers/auth.provider.tsx
@@ -23,12 +23,7 @@ export default function AuthProvider({ children }: Readonly<Props>) {
     setCookie('email', me.email);
   }
 
-  if (
-    !me &&
-    pathname !== '/login' &&
-    pathname !== '/password/forgot' &&
-    pathname !== '/password/reset'
-  ) {
+  if (!me) {
     return redirect(redirectUrl);
   }
 
