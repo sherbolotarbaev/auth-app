74bb3b9c9f72ff5c3aa08a1eef6f70537001a2ea
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-03-13 13:05:59 +0600

    fixed bug

--- app/redux/api/me/server.ts
+++ app/redux/api/me/server.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 import axios from '@/app/redux/api/axios';
 
 export async function getMe(_req: GetMeRequest): Promise<User | 401 | undefined> {
-  if (!cookies().get('session-middleware')) return;
+  if (!cookies().get('token')) return;
 
   try {
     const response = await axios({ url: '/me', method: 'GET' });
