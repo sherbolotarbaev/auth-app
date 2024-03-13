79dd6e57a7a6216d895de77277666f068981d1b3
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-03-13 13:15:38 +0600

    update: logic

--- app/redux/api/me/server.ts
+++ app/redux/api/me/server.ts
@@ -3,15 +3,13 @@ import { cookies } from 'next/headers';
 import axios from '@/app/redux/api/axios';
 
 export async function getMe(_req: GetMeRequest): Promise<User | 401 | undefined> {
-  if (!cookies().get('token')) {
-    cookies().delete('session-middleware');
-    return;
-  }
+  if (!cookies().get('token')) return;
 
   try {
     const response = await axios({ url: '/me', method: 'GET' });
     return response.data;
   } catch (error: any) {
+    cookies().delete('session-middleware');
     return 401;
   }
 }
